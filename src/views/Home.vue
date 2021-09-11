34bfad94c2575c6ec00327ac51744777e371b48a
huangshen <1426887150@qq.com>
2021-09-11 16:51:00 +0800

    前端公告板实现

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
-    <div class="box">🔔 {{billboard}} </div>
+    <div class="box">🔔 {{ billboard.content }} </div>
   </div>
 </template>
 
 <script>
+import { getBillboard } from "@/api/billboard";
 
 export default {
   name: "Home",
   data(){
     return{
-      billboard: '版本更新'
+      billboard: {
+        content: ''
+      }
+    }
+  },
+  created() {
+    this.fetchBillboard()
+  },
+  methods: {
+    async fetchBillboard(){
+      getBillboard().then((result) => {
+        const { data } = result
+        this.billboard = data
+      }).catch((err) => {
+        
+      });
     }
   }
 };
