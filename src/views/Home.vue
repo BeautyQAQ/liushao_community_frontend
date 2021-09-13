28245d90df32ad86891a7d9557d2fadfbdca9514
huangshen <1426887150@qq.com>
2021-09-13 22:03:11 +0800

    每日一句

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,44 @@
 <template>
   <div>
-    <div class="box">🔔 {{ billboard.content }} </div>
+    <div class="box">🔔 {{ billboard.content }}</div>
+    <div class="columns">
+      <div class="column is-three-quarters">
+        <TopicList></TopicList>
+      </div>
+      <div class="column">
+        <card-bar></card-bar>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { getBillboard } from "@/api/billboard";
+import CardBar from "@/views/card/CardBar";
+import PostList from "@/views/post/Index";
 
 export default {
   name: "Home",
-  data(){
-    return{
+  components: { CardBar, TopicList: PostList },
+  data() {
+    return {
       billboard: {
-        content: ''
-      }
-    }
+        content: "",
+      },
+    };
   },
   created() {
-    this.fetchBillboard()
+    this.fetchBillboard();
   },
   methods: {
-    async fetchBillboard(){
-      getBillboard().then((result) => {
-        const { data } = result
-        this.billboard = data
-      }).catch((err) => {
-        
-      });
-    }
-  }
+    async fetchBillboard() {
+      getBillboard()
+        .then((result) => {
+          const { data } = result;
+          this.billboard = data;
+        })
+        .catch((err) => {});
+    },
+  },
 };
 </script>
---
+++ src/views/card/Tip.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-card class="box-card" shadow="never">
+    <div slot="header">
+      <span>🥳 每日一句</span>
+    </div>
+    <div>
+      <div class="has-text-left block">
+        {{ tip.content }}
+      </div>
+      <div class="has-text-right mt-5 block">
+        ——{{ tip.author }}
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+import { getTodayTip } from '@/api/tip'
+
+export default {
+    name: 'Tip',
+    data() {
+        return {
+            tip: {}
+        }
+    },
+    created() {
+        this.fetchTodayTip()
+    },
+    methods: {
+        fetchTodayTip(){
+            getTodayTip().then((result) => {
+                const { data } = result
+                this.tip = data
+            }).catch((err) => {
+                
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
