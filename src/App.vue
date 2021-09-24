ac453128fae33c5bb5d803eb029551a31993963f
huang丶shen <1426887150@qq.com>
2021-09-24 08:46:36 +0000

    前端header实现01

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="container context">
-    <router-view />
+  <div>
+    <div class="mb-5">
+      <Header></Header>
+    </div>
+    <div class="container context">
+      <router-view :key="this.$route.fullPath"></router-view>
+    </div>
   </div>
 </template>
+
+<script>
+import Header from "@/components/Layout/Header";
+
+export default {
+  name: "APP",
+  components: {Header}
+}
+</script>
 
 <style scoped>
 .container {
