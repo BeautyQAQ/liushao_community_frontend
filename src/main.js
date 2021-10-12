a9c1dbfb431e40105b4f135df203dc2bb261b2ac
huang丶shen <1426887150@qq.com>
2021-10-12 06:05:03 +0000

    帖子列表+分页

--- src/main.js
+++ src/main.js
@@ -9,7 +9,20 @@ import 'buefy/dist/buefy.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/app.css'
+import './assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css'
+
 import "@/permission";
+import relativeTime from 'dayjs/plugin/relativeTime';
+// 国际化
+import 'dayjs/locale/zh-cn'
+const dayjs = require('dayjs');
+// 相对时间插件
+dayjs.extend(relativeTime)
+
+dayjs.locale('zh-cn') // use locale globally
+dayjs().locale('zh-cn').format() // use locale in a specific instance
+
+Vue.prototype.dayjs = dayjs;//可以全局使用dayjs
 
 Vue.use(Buefy)
 Vue.use(ElementUI)
